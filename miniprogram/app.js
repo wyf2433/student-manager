b85f7e866fc18ca9e53bb5ad409bbe7567bf965b
wyf2433 <2433905249@qq.com>
2026-07-16 05:10:07 +0800

    修复云开发环境ID为cloud1-d3gde4cs1fa50b701

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,7 +4,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'student-manager',
+        env: 'cloud1-d3gde4cs1fa50b701',
         traceUser: true,
       })
     }
